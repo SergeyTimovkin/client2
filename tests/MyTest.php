06644ede53afc32ed7df3d34d4ecbf06e70af722
R13Sergey <r13sergey@gmail.com>
2021-07-20 09:08:33 +0300

    Установил phpunit-json-assertions

--- tests/MyTest.php
+++ tests/MyTest.php
@@ -1,11 +1,17 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use EnricoStahn\JsonAssert\Assert as JsonAssert;
 
 class MyTest extends TestCase
 {
+    use JsonAssert;
     public function testMy(): void
     {
+        $json = json_decode('{"foo":1}');
+
+      //  $this->assertJsonMatchesSchema($json, './my-schema.json');
+        $this->assertJsonValueEquals(1, '* | [0]', $json);
         $this->assertTrue(true);
     }
 }
